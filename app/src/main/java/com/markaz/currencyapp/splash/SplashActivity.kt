bf9293626f2a3683be1101e75d368212cd5158e9
Bilal-Pakwheels <bilal.khalid@pakeventures.com>
2023-10-02 01:09:12 +0500

    initial commit

---
+++ app/src/main/java/com/markaz/currencyapp/splash/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.markaz.currencyapp.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.lifecycle.lifecycleScope
+import com.markaz.currencyapp.MainActivity
+import kotlinx.coroutines.delay
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity: ComponentActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launchWhenCreated {
+            delay(3000)
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+    }
+
+
+}
